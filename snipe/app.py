4a8674e422ebbdce24b2e43884ab8d95fff87e49
Javed Khan <javed@agiliq.com>
2013-10-25 11:25:10 +0530

    added arrow, overlay function, mouse events

--- snipe/app.py
+++ snipe/app.py
@@ -4,12 +4,15 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
+from snipe import res
+
 
 class SnipeApp(QtGui.QWidget):
 
     def __init__(self):
         super(SnipeApp, self).__init__()
 
+        self.pixmap = None
         self.initUI()
 
     def initUI(self):
@@ -17,18 +20,33 @@ class SnipeApp(QtGui.QWidget):
         self.resize(1360, 760)
 
         hbox = QtGui.QHBoxLayout(self)
-        pixmap = self.getScreen()
-        pixmap = pixmap.scaled(self.size(), QtCore.Qt.KeepAspectRatio)
+        self.pixmap = self.getScreen()
+        self.pixmap = self.pixmap.scaled(self.size(), QtCore.Qt.KeepAspectRatio)
 
-        lbl = QtGui.QLabel(self)
-        lbl.setPixmap(pixmap)
+        self.lbl = QtGui.QLabel(self)
+        self.lbl.setPixmap(self.pixmap)
 
-        hbox.addWidget(lbl)
+        hbox.addWidget(self.lbl)
         self.setLayout(hbox)
 
         self.move(300, 200)
         self.setWindowTitle('Snipe')
         self.showMaximized()
+
+    def mousePressEvent(self, event):
+        print "pressed at %s, %s" % (event.pos().x(), event.pos().y())
+
+    def mouseReleaseEvent(self, event):
+        print "released at %s, %s" % (event.pos().x(), event.pos().y())
+
+    def mouseMoveEvent(self, event):
+        print "moved at %s, %s" % (event.pos().x(), event.pos().y())
+
+    def drawArrow(self, a, b):
+        qpixmappainter = QtGui.QPainter(self.pixmap)
+        arrow = QtGui.QPixmap(":/images/arrow.svg")
+        qpixmappainter.drawPixmap(a, b, arrow);
+        self.lbl.setPixmap(self.pixmap)
 
     def getScreen(self):
         return QtGui.QPixmap.grabWindow(
